1919d06c2d744011d4c9a5054266717bb271f475
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-05-08 17:01:06 -0700

    adjusted css for mobile added media queries for desktop and added functionality so that side drawer disappears when backdrop is clicked

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,10 +3,20 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/AuxFile';
+
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+        <Backdrop show={props.open} clicked={props.closed} />
+        <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
 
@@ -15,6 +25,7 @@ const sideDrawer = (props) => {
                 <NavigationItems />
             </nav>
         </div>
+        </Aux>
     );
 
 }
