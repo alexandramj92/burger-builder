5d0eb7b334f2df20f7ca33e437233e444c7dfa2f
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-05-22 10:35:19 -0700

    added contact data form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -63,34 +63,13 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!')
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Alexandra Jackson',
-        //         address: {
-        //             street: 'Test Street',
-        //             zipCode: '90212',
-        //             country: 'United States'
-        //         },
-        //         email: 'test@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     } )
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     } );
+
         const queryParams = [];
 
         for ( let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         
         this.props.history.push({
